12c5230f9bf2749d5e3ce97f363edbb20f26929c
Abhishek <abhishek@pop-os.localdomain>
2022-07-21 10:23:50 +0530

    Added Whack a Mole game

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -4,43 +4,36 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-    // clear any existing timers
     clearInterval(countdown);
-
     const now = Date.now();
     const then = now + seconds * 1000;
-    // console.log({now , then});
     displayTimeLeft(seconds);
     displayEndTime(then);
+    console.log({now, then});
 
     countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
-        // check if we should stop it!
-        if(secondsLeft <= 0) {
+        const convertedSeconds = Math.round((then - Date.now()) / 1000);
+        if(convertedSeconds < 0) {
             clearInterval(countdown);
-            return;
+            return; // It will stop the function from running;
         }
-        // display it
-        displayTimeLeft(secondsLeft);
+        displayTimeLeft(convertedSeconds);
     }, 1000);
 }
 
 function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`; 
     timerDisplay.textContent = display;
     document.title = display;
-    // console.log({minutes, remainderSeconds});
 }
 
-
 function displayEndTime(timestamp) {
-    const end = new Date(timestamp);
-    console.log({end});
-    const hour = end.getHours();
-    const minutes = end.getMinutes();
-    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}${hour > 12 ? 'pm' : 'am'}`; 
+    const end = new Date(timestamp); 
+    const hours = end.getHours();
+    const minutes = end.getMinutes(); 
+    endTime.textContent = `Be Back At ${hours > 12 ? hours - 12 : hours}:${minutes < 10 ? '0' : ''}${minutes}${hours > 12 ? 'pm' : 'am'}`;
 }
 
 function startTimer() {
@@ -52,6 +45,11 @@ buttons.forEach(button => button.addEventListener('click', startTimer));
 document.customForm.addEventListener('submit', function(e) {
     e.preventDefault();
     const mins = this.minutes.value;
-    timer(mins * 60);
+    const convertedMins = parseInt(mins);
+    timer(convertedMins * 60);
     this.reset();
 })
+
+
+
+
